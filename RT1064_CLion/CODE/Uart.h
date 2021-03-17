658967c19da9542a20c33fddf9ef4f1b6215a865
sugarlxn <72424705+sugarlxn@users.noreply.github.com>
2021-03-17 15:27:41 +0800

    Update Uart.h

--- RT1064_CLion/CODE/Uart.h
+++ RT1064_CLion/CODE/Uart.h
@@ -5,4 +5,14 @@
 #ifndef RT1064_CODE_UART_H_
 #define RT1064_CODE_UART_H_
 
+
+void example_uart_callback(LPUART_Type *base, lpuart_handle_t *handle, status_t status, void *userData);
+void send_steer_data(void);
+void send_motor_data(void);
+
+
+
+
+
+
 #endif // RT1064_CODE_UART_H_
